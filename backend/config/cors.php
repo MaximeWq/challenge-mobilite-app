c4eb1a02bdb94be7101ccd696ba0ba2830effe78
Maxime Wissocq <89657470+MaximeWq@users.noreply.github.com>
2025-07-19 13:57:42 +0200

    Ajout fichier de config cors

---
+++ backend/config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://frontend-production-f587.up.railway.app/',
+        // Ajoute ici d'autres domaines si besoin
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
